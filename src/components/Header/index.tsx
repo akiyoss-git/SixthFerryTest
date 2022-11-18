d1c2e5ebd86a051dce43ce4308e9749371d6ca70
akiyoss <sawerford10@gmail.com>
2022-11-18 16:17:55 +0300

    Components files

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>I'm header</>
+  )
+}
+
+export default Header;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>I'm Main</>
+  )
+}
+
+export default Main;
